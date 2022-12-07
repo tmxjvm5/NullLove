11cbf724026c4bf5c6459db6fc38507c19cfba7a
PRH <qkrfogh789@naver.com>
2022-12-07 17:03:38 +0900

    카카오 로그인 진짜가능

--- src/main/java/com/loven/controller/LoginController.java
+++ src/main/java/com/loven/controller/LoginController.java
@@ -3,22 +3,12 @@ package com.loven.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.loven.entity.OAuthToken;
 import com.loven.service.BoardServiceImpl;
 import com.loven.service.MemberService;
 import com.loven.service.OAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +18,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.loven.entity.Company;
 import com.loven.entity.User;
 import com.loven.service.BoardService;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
@@ -139,82 +128,6 @@ public class LoginController {
 	
 
 	//---------------------------------여기서부터 카카오 login Api--------------------------------------------
-//	@GetMapping("/auth/kakao/callback")
-//	@ResponseBody
-//	public void kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수
-//
-//		// POST방식으로 key=value 데이터를 요청(카카오쪽으로)
-//		//Restrofit2
-//		//OkHttp
-//		//RestTemplate
-//
-//		RestTemplate rt = new RestTemplate();
-//
-//		//HttpHeader 오브젝트 생성
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-//
-//		//HttpBody 오브젝트 생성
-//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//		params.add("grant_type", "authorization_code");
-//		params.add("client_id", "2e5066b51786f5af82c6c4222bed1517");
-//		params.add("redirect_uri", "http://localhost:8088/auth/kakao/callback");
-//		params.add("code", code);
-//
-//		//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-//		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-//				new HttpEntity<>(params, headers);
-//
-//		//Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
-//		ResponseEntity<String> response = rt.exchange(
-//				"https://kauth.kakao.com/oauth/token",
-//				HttpMethod.POST,
-//				kakaoTokenRequest,
-//				String.class
-//		);
-//		// Gson, Json Simple, ObjectMapper
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		OAuthToken oauthToken = null;
-//
-//		try {
-//			oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-////            System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
-//
-//			String ac_token = oauthToken.getAccess_token();
-//			System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
-//			//return response.getBody();
-//
-//			HashMap<String, String> info = oAuthService.getUserInfo(ac_token);
-//
-//			String id = info.get("id");
-//			String name = info.get("nickname");
-//			String email = info.get("email");
-//
-//			//
-//
-//			if (info == null){
-//				System.out.println(id);
-//				System.out.println(name);
-//				System.out.println(email);
-//
-//			}
-//
-//			User vo = new User();
-//			vo.setId(id);
-//			if(email!=null){
-//				vo.setEmail(email);
-//			}else{
-//				vo.setEmail(id);
-//			}
-//			vo.setNick(name);
-//			vo.setName(name);
-//			memberService.kakaoLogin(vo);
-//
-//			} catch (JsonProcessingException e) {
-//			throw new RuntimeException(e);
-//		}
-// >>>  밑부터 성민씨 코드 이상하게 수정해논상태  >>>>
-
 
 	@RequestMapping(value = "/klogin")
 	public String login(@RequestParam(value = "code", required = false) String code, HttpSession session, HttpServletResponse response) throws Exception{

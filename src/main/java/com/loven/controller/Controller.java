ae6c17e1cb485e45d278c5595743aa16242ee9e7
tmxjvm5 <tmxjvm5@naver.com>
2022-12-08 09:19:40 +0900

    추천페이지뷰

--- src/main/java/com/loven/controller/Controller.java
+++ src/main/java/com/loven/controller/Controller.java
@@ -2,6 +2,7 @@ package com.loven.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -43,7 +44,10 @@ public class Controller {
         return "write";
     }
     // 글쓰기 폼
-
+    @RequestMapping("/searchrs")
+    public String searchrs(){
+        return "searchrs";
+    }
     }
 
 
